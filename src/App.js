95dcfee70234de313ecfb8f7984ad13f9c922f3f
hendikajulbahri <julbahrih@gmail.com>
2018-12-11 15:48:12 +0700

    commit1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state ={
+        setup: '',
+        punchline: ''
+    }
+}
+
+
   render() {
+    let reload = () => {
+      let url = `https://safe-falls-22549.herokuapp.com/random_joke`
+      axios.get(url)
+      .then((ambil) => {
+          this.setState({
+              setup: ambil.data.setup,
+              punchline: ambil.data.punchline
+          })
+      })
+  }
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+<div className="container">
+  <center><div class="card col-6 kartu">
+  <div class="card-header">
+    Dad Jokes
+  </div>
+  <div class="card-body">
+    <h3 class="card-title">{this.state.setup}</h3>
+    <p class="card-text">{this.state.punchline} </p>
+    <a href="#" class="btn btn-primary tombol" onClick={reload}>Reload</a>
+  </div>
+</div></center>
+</div>
+
+
     );
   }
 }
